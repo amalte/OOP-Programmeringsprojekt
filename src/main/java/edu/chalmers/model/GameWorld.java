fc4cc98cea6c6bd3fc65a4d73095fef017f7c64a
Voczi <62214162+Voczi@users.noreply.github.com>
2020-09-09 14:54:34 +0200

    * Amend JavaDoc for constructors * Add the ability to modify: - Background texture - List of entities

--- src/main/java/edu/chalmers/model/GameWorld.java
+++ src/main/java/edu/chalmers/model/GameWorld.java
@@ -5,6 +5,7 @@ import com.almasb.fxgl.texture.Texture;
 
 import javax.swing.text.html.parser.Entity;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -12,14 +13,18 @@ import java.util.List;
  */
 public class GameWorld {
     private static final double DEFAULT_PPM = 16;
+    private static final int MAX_ENTITIES = 256;
 
     private final PhysicsWorld physicsWorld;
     private final int appHeight;
     private final double ppm;
 
+    private List<Entity> entityList = new ArrayList<>();
+    private Texture backgroundTexture;
+
     /**
      * Primary constructor for GameWorld.
-     * Uses {@see com.almasb.fxgl.physics.PhysicsWorld} through delegation. Instantiates the aforementioned class with the parameters for this constructor.
+     * Uses class PhysicsWorld through delegation. Instantiates the aforementioned class with the parameters for this constructor.
      * @param appHeight The height of the app
      * @see com.almasb.fxgl.physics.PhysicsWorld
      */
@@ -31,7 +36,7 @@ public class GameWorld {
 
     /**
      * Secondary constructor for GameWorld. Allows you to set the "PPM"-value.
-     * Uses {@see com.almasb.fxgl.physics.PhysicsWorld} through delegation. Instantiates the aforementioned class with the parameters for this constructor.
+     * Uses PhysicsWorld through delegation. Instantiates the aforementioned class with the parameters for this constructor.
      * @param appHeight The height of the app
      * @param ppm Pixels per meter
      * @see com.almasb.fxgl.physics.PhysicsWorld
@@ -40,5 +45,79 @@ public class GameWorld {
         this.appHeight = appHeight;
         this.ppm = ppm;
         physicsWorld = new PhysicsWorld(appHeight, ppm);
+    }
+
+    /**
+     * @param tpf Time per frame
+     */
+    public void onUpdate(double tpf)
+    {
+        physicsWorld.onUpdate(tpf);
+    }
+
+    /**
+     * Sets the new background of the GameWorld. The height of the texture has to be equal to GameWorld.appHeight.
+     * @param backgroundTexture The new background of the GameWorld
+     * @return True if backgroundTexture was not null and backgroundTexture.getHeight() was equal to GameWorld.appHeight.
+     */
+    public boolean setBackgroundTexture(Texture backgroundTexture)
+    {
+        if (backgroundTexture != null && backgroundTexture.getHeight() == appHeight)
+        {
+            this.backgroundTexture = backgroundTexture;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Gets the background texture for the world.
+     * @return The current background texture of the world.
+     */
+    public Texture getBackgroundTexture()
+    {
+        return backgroundTexture;
+    }
+
+    /**
+     * Adds an entity to the world.
+     * @param entity The entity to add
+     * @return Whether or not MAX_ENTITIES was exceeded and the entity was not added.
+     */
+    public boolean addEntity(Entity entity)
+    {
+        if (entityList.size() < MAX_ENTITIES)
+        {
+            entityList.add(entity);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Removes an entity from the world.
+     * @param entity The entity to remove
+     * @return Whether or not the entity existed in the first place.
+     */
+    public boolean removeEntity(Entity entity)
+    {
+        if (entityList.contains(entity))
+        {
+            entityList.remove(entity);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Gets the iterator for the list of entities.
+     * @return Iterator for the list of entities.
+     */
+    public Iterator<Entity> getEntityIterator()
+    {
+        return entityList.iterator();
     }
 }
