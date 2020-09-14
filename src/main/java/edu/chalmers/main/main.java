81960dd0164cddb19c6545b836d7ba5e0a5ac970
BuildTools <unconfigured@null.spigotmc.org>
2020-09-14 13:55:33 +0200

    test

--- src/main/java/edu/chalmers/main/main.java
+++ src/main/java/edu/chalmers/main/main.java
@@ -3,9 +3,12 @@ package edu.chalmers.main;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.time.Timer;
+import com.almasb.fxgl.time.TimerAction;
 import edu.chalmers.controller.Controller;
 import edu.chalmers.model.GameWorldFactory;
 import edu.chalmers.model.Player;
+
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
 
 public class main extends GameApplication {
