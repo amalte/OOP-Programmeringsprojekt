7817aebd5c6ef62939078ac10b94b3c5d27f21db
Voczi <62214162+Voczi@users.noreply.github.com>
2020-09-14 13:28:57 +0200

    Use default return values for overrided methods (from FXGL wiki)

--- src/main/java/edu/chalmers/view/main/MainMenu.java
+++ src/main/java/edu/chalmers/view/main/MainMenu.java
@@ -1,10 +1,13 @@
 package edu.chalmers.view.main;
 
-import com.almasb.fxgl.app.scene.FXGLMenu;
-import com.almasb.fxgl.app.scene.MenuType;
+import com.almasb.fxgl.app.scene.*;
 import javafx.beans.binding.StringBinding;
-import javafx.scene.Node;
+import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.*;
 import org.jetbrains.annotations.NotNull;
 
 public class MainMenu extends FXGLMenu {
@@ -15,37 +18,37 @@ public class MainMenu extends FXGLMenu {
 
     @NotNull
     @Override
-    protected Button createActionButton(@NotNull StringBinding stringBinding, @NotNull Runnable runnable) {
-        return null;
+    protected Button createActionButton(@NotNull StringBinding name, @NotNull Runnable action) {
+        return new Button(name.get());
     }
 
     @NotNull
     @Override
-    protected Button createActionButton(@NotNull String s, @NotNull Runnable runnable) {
-        return null;
+    protected Button createActionButton(@NotNull String name, @NotNull Runnable action) {
+        return new Button(name);
     }
 
     @NotNull
     @Override
-    protected Node createBackground(double v, double v1) {
-        return null;
+    protected Node createBackground(double width, double height) {
+        return new Rectangle(width, height, Color.GRAY);
     }
 
     @NotNull
     @Override
-    protected Node createProfileView(@NotNull String s) {
-        return null;
+    protected Node createProfileView(@NotNull String profileName) {
+        return new Text(profileName);
     }
 
     @NotNull
     @Override
-    protected Node createTitleView(@NotNull String s) {
-        return null;
+    protected Node createTitleView(@NotNull String title) {
+        return new Text(title);
     }
 
     @NotNull
     @Override
-    protected Node createVersionView(@NotNull String s) {
-        return null;
+    protected Node createVersionView(@NotNull String version) {
+        return new Text(version);
     }
 }
