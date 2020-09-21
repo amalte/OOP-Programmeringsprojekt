5e0c4f12ebab16b734a62a6ff3a4b546444c309f
Voczi <62214162+Voczi@users.noreply.github.com>
2020-09-21 15:12:37 +0200

    Finish JavaDocs for the ActionButton class.

--- src/main/java/edu/chalmers/view/main/ActionButton.java
+++ src/main/java/edu/chalmers/view/main/ActionButton.java
@@ -7,13 +7,31 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+/**
+ * A clickable StackPane, with a default background and text.
+ */
 class ActionButton extends StackPane {
+    /**
+     * The width of the action button.
+     */
     public static final double BUTTON_WIDTH = 240;
 
+    /**
+     * The height of the action button.
+     */
     public static final double BUTTON_HEIGHT = 48;
 
+    /**
+     * The font size of the text inside of the action button.
+     */
     public static final double BUTTON_FONT_SIZE = 20;
 
+    /**
+     * Default constructor.
+     * Creates controls for manipulating settings and binds the action to mouse clicks.
+     * @param buttonText The text of the button
+     * @param action The action of the button. It is ran when the button is clicked.
+     */
     public ActionButton(String buttonText, Runnable action) {
         this.createControls(buttonText);
         this.setOnMouseClicked(mouseEvent -> action.run());
@@ -21,6 +39,10 @@ class ActionButton extends StackPane {
         this.setPrefHeight(BUTTON_HEIGHT);
     }
 
+    /**
+     * Creates a background and a text for the button.
+     * @param buttonText The text of the button
+     */
     private void createControls(String buttonText)
     {
         Rectangle background = new Rectangle(BUTTON_WIDTH, BUTTON_HEIGHT);
