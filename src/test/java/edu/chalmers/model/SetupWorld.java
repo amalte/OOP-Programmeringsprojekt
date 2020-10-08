b5f733d99de9ce74f81c8d08f075b64030595e98
sam123bir <sam.salek@hotmail.com>
2020-10-08 21:06:28 +0200

    Added tests for EnemyComponent. Fixed an issue with the test thread.

--- src/test/java/edu/chalmers/model/SetupWorld.java
+++ src/test/java/edu/chalmers/model/SetupWorld.java
@@ -6,19 +6,28 @@ import edu.chalmers.main.Main;
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
 
 public class SetupWorld {       // Initialize world to get access to library methods.
+
+    // Variable keeps track of the applications launch state.
+    // This is used to avoid launching the application twice or starting the thread multiple times (which gives errors).
+    private static boolean hasLaunched = false;
+
     public static void initApp() throws InterruptedException {
 
         Thread t = new Thread(() ->
         {
             GameApplication.launch(Main.class, new String[0]);
         });
-        t.start();
 
-        Thread.sleep(1000);
-        System.out.println("App thread started");
+        // Start thread if it hasn't already.
+        if(!hasLaunched) {
+            hasLaunched = true;
 
+            t.start();
+            Thread.sleep(1000);
+            System.out.println("App thread started");
 
-        // Initialize before tests \\
-        getGameWorld().addEntityFactory(new GameWorldFactory());
+            // Initialize before tests \\
+            getGameWorld().addEntityFactory(new GameWorldFactory());
+        }
     }
 }
