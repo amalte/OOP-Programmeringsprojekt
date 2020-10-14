b074c97af480a319b5b1110cb7ba3506751bed6f
62214162+Voczi@users.noreply.github.com <62214162+Voczi@users.noreply.github.com>
2020-10-14 14:41:51 +0200

    Add ActionButton to nodes sub-package and allow the background image of the button to be changed.

---
+++ src/main/java/edu/chalmers/view/nodes/ActionButton.java
@@ -0,0 +1,95 @@
+package edu.chalmers.view.nodes;
+
+import com.almasb.fxgl.dsl.FXGL;
+import edu.chalmers.utilities.Constants;
+import javafx.beans.binding.Bindings;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+import static edu.chalmers.view.util.ViewUtil.addNode;
+
+/**
+ * A clickable StackPane, with a default background and text.
+ */
+public class ActionButton extends StackPane {
+    /**
+     * The width of the action button.
+     */
+    public static final double BUTTON_WIDTH = 240;
+
+    /**
+     * The height of the action button.
+     */
+    public static final double BUTTON_HEIGHT = 48;
+
+    /**
+     * The font size of the text inside of the action button.
+     */
+    public static final double BUTTON_FONT_SIZE = 20;
+
+    private String tag;
+    private String backgroundPath;
+
+    /**
+     * Default constructor.
+     * Creates controls for manipulating settings and binds the action to mouse clicks.
+     * @param buttonText The text of the button
+     * @param action The action of the button. It is ran when the button is clicked.
+     */
+    public ActionButton(String buttonText, Runnable action) {
+        this.createControls(buttonText);
+        this.setOnMouseClicked(mouseEvent -> action.run());
+        this.setPrefWidth(BUTTON_WIDTH);
+        this.setPrefHeight(BUTTON_HEIGHT);
+    }
+
+    public ActionButton(String buttonText, Runnable action, String backgroundPath)
+    {
+        this.backgroundPath = backgroundPath;
+        this.createControls(buttonText);
+        this.setOnMouseClicked(mouseEvent -> action.run());
+        this.setPrefWidth(BUTTON_WIDTH);
+        this.setPrefHeight(BUTTON_HEIGHT);
+    }
+
+    /**
+     * Creates a background and a text for the button.
+     * @param buttonText The text of the button
+     */
+    private void createControls(String buttonText)
+    {
+        if (this.backgroundPath != null && !this.backgroundPath.trim().isEmpty())
+        {
+            ImageView backgroundImage = new ImageView(new Image(backgroundPath, BUTTON_WIDTH, BUTTON_HEIGHT, false, false));
+            getChildren().add(backgroundImage);
+
+            Text text = FXGL.getUIFactoryService().newText(buttonText, Color.BLACK, BUTTON_FONT_SIZE * 2.5);
+            text.setStyle("-fx-font-weight: bold;");
+            getChildren().add(text);
+        } else {
+            Rectangle background = new Rectangle(BUTTON_WIDTH, BUTTON_HEIGHT);
+            background.setStroke(Color.GREEN);
+            background.fillProperty().bind(
+                    Bindings.when(hoverProperty()).then(Color.LIGHTGREEN).otherwise(Color.GREEN)
+            );
+            getChildren().add(background);
+
+            Text text = FXGL.getUIFactoryService().newText(buttonText, Color.BLACK, BUTTON_FONT_SIZE);
+            getChildren().add(text);
+        }
+    }
+
+    public void setTag(String tag)
+    {
+        this.tag = tag;
+    }
+
+    public String getTag()
+    {
+        return this.tag;
+    }
+}
