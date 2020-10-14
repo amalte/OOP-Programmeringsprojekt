56be27e633eec010539f30f1ffc474ac665202a1
62214162+Voczi@users.noreply.github.com <62214162+Voczi@users.noreply.github.com>
2020-10-14 17:30:55 +0200

    Comment GameMenuType

--- src/main/java/edu/chalmers/controller/GameMenuType.java
+++ src/main/java/edu/chalmers/controller/GameMenuType.java
@@ -1,5 +1,26 @@
 package edu.chalmers.controller;
 
+/**
+ * Types of menus.
+ */
 public enum GameMenuType {
-    Main, Settings, Play, Exit
+    /**
+     * Main menu.
+     */
+    Main,
+
+    /**
+     * Settings menu.
+     */
+    Settings,
+
+    /**
+     * Play menu.
+     */
+    Play,
+
+    /**
+     * Exit menu.
+     */
+    Exit
 }
