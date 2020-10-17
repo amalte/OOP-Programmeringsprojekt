f3247e35cd9f9ba11721143ac2ed1eea7fc2ff28
oscarvid <o.j.arvidson@hotmail.com>
2020-10-17 12:54:07 +0200

    Created a GameOverView and a GameOverViewController and added the to main.

---
+++ src/main/java/edu/chalmers/controller/game/GameOverViewController.java
@@ -0,0 +1,52 @@
+package edu.chalmers.controller.game;
+
+import edu.chalmers.controller.GameMenuType;
+import edu.chalmers.controller.MenuController;
+import edu.chalmers.main.Main;
+import edu.chalmers.model.GenericPlatformer;
+import edu.chalmers.model.IObserver;
+import edu.chalmers.view.game.GameOverView;
+
+import static com.almasb.fxgl.dsl.FXGL.getGameScene;
+
+/**
+ * The controller for the exit menu view.
+ */
+public class GameOverViewController extends MenuController<GameOverView> implements IObserver {
+
+    private GenericPlatformer game;
+
+    /**
+     * Default constructor for ExitMenuController.
+     * @param viewInstance Instance of a view to associate the controller with.
+     * @param mainInstance An instance of the Main class.
+     */
+    public GameOverViewController(GameOverView viewInstance, Main mainInstance, GenericPlatformer game)
+    {
+        super(viewInstance, mainInstance, GameMenuType.GameOver);
+        this.game = game;
+    }
+
+    /**
+     * Initialize the nodes (make view create them, binds actions to them, etc.)
+     */
+    @Override
+    protected void initializeNodes()
+    {
+        super.initializeNodes();
+
+        getViewInstance().getExitButton().setOnMousePressed(mouseEvent -> {
+            getGameScene().getRoot().getScene().setOnKeyPressed(keyEvent -> { });
+            this.hide();
+
+            getMainInstance().stopGame();
+        });
+    }
+
+    @Override
+    public void update() {
+        if(game.getPlayerComponent().getHealth() <= 0){
+            this.show();
+        }
+    }
+}
